e98aba6e0540f388366c3db93a3746d90aaa1fbf
jenward <jenforce@aol.com>
2016-06-10 17:15:26 -0400

    Friday version gif load working

--- javascript/game.js
+++ javascript/game.js
@@ -1,91 +1,66 @@
  $(document).ready(function() {
- 
- $('#gameshow, #endjumbotron').hide('slowly');//prep for game start screen
+var gifThings=[];
 
+    $('#thingSubmit').on('click', function() {
+        var thing = $('#thing-input').val();
+          console.log(thing);
+          
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + thing + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-$('#startjumbotron').click(function() {     //start game and timer
-    	$('#jumbotronone').hide('slowly');
-		  $('#gameshow').show('slowly');
-		      questionGenerator(); 
-          timer();
-       
-});          
+ $.ajax({url: queryURL, method: 'GET'})         
+    .done(function(response) {
+         var results = response.data;
+         for(var i=0; i < results.length; i++){
+            if (results[i].rating == "r" || results[i].rating == "pg-13"){
+            }
+            else {
+            var gifDiv = $('<div class="item">');
+             var rating = results[i].rating;
+             var p = $('<p>').text( "Rating: " + rating);
+             var personImage = $('<img>');
+             var gifId = results[i].id;
+              gifThings.push(gifId);
+              console.log(gifId); 
+personImage.attr('src', results[i].images.fixed_height.url);
+//personImage.attr('src', results[i].images.original_still.url);
+    console.log(results[i].images.original_still.url);
 
-function timer(){               //timer 
-	 var counter = 8; 
-         var intervalId = setInterval(function() {              
-              counter--;
-         var countdown = document.getElementById("tContainer"); 
-             countdown.innerHTML = counter;     //timer to screen
-
-          if (counter === 0) {    
-              clearInterval(intervalId);
-               console.log("timer end");
-               endOfGame();
-              }
-              }, 1000);
-  }; //end of timer 
-
-function questionGenerator(){   
-      var ques = [
-        ["0.0 one jhrkjehflkjhddqjkjjriejqe","0 .1correct","answer1","answer2","answer3"],
-         ["1.0two jhrkjehflkjhddqjkjjriejqe","1.1 correct","answer1","answer2","answer3"],
-       ];
-       console.log(ques.length);
-
-
-            var quesbox = $('<div class="questionbox">');  
-            var questiontxt = ques[0] [0];
-            quesbox.append(questiontxt); 
-
-            var ansbox = $('<div class="answerbox">');  
-            var answercorrect = ques[0][1]; 
-            var answertxt1 = ques[0][2]; 
-            var answertxt2 = ques[0][3]; 
-            var answertxt3 = ques[0][4];
-            ansbox.append(answercorrect, answertxt1, answertxt2,answertxt3);   
-
-           $("#qContainer").append(quesbox, ansbox);
-  };        
-  
-
-
-
-function endOfGame(){
-
-    $('#gameshow').hide('slowly');
-    $('#endjumbotron').show('slowly');
-
-};
+             gifDiv.append(p)
+             gifDiv.append(personImage) 
+             $('#gifsAppearHere').prepend(gifDiv);    
+        
+            }
+         }
+    }); 
+    return false;
+});
 
 });
 
 /*
-function appendText() {
-    var txt1 = "<p>Text.</p>";              // Create text with HTML
-    var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
-    var txt3 = document.createElement("txt3cnt");
-    txt3.innerHTML = " created element 1Text";         
 
-    $("#gContainer").append(txt1, txt2, txt3);     
-  }
-//var questions = []; //stores questions and answers 
-
-//unction questionGenerator{
-//    var txt3 = document.createElement("p");
-  //  txt3.innerHTML = "Text.";   
-
-
-//}// writes questions and answers to screen, random order to questions, random order to answers right answer flage radio buttons for loops and 
-
-//questionTimer{}
-
-//
-
-//gameScore{}
+        // Going to the URL and getting the data; 
+        $.ajax({url: queryURL, method: 'GET'})
+        .done(function(response) {
+                // Setting the variable of image_url to the specific object 
+            var results = response.data;                
+            var gifDiv = $('<div class="item">')
+            var rating = results.rating;
+            var p = $('<p>').text( "Rating: " + rating);
+            var personImage = $('<img>');
+             personImage.attr('src', results.images.fixed_height.url);
+             gifDiv.append(p)
+             gifDiv.append(personImage)
+             $('#videoContainer').prepend(gifDiv);               
 
 
-//show hide new element for total score, correct incorrect answer totals button for new game
-/*var triviaQuestions = {question:"question question question question question question question question question ", correct:"correct", choice1:"c1"choice2:"c2"choice3:"c3"};
-$("#gContainer").append(txt1, txt2, txt3);     // Append new elements
-}*/
+
+
+                //$('#videoContainer').prepend(thingImage);
+            });
+    });
+*/
+
+//http://api.giphy.com/v1/gifs/search?q=matthew%20goode&api_key=dc6zaTOxFJmzC&limit=10
+
+//<td>http://api.giphy.com/v1/gifs/search?q=waterfalls&amp;api_key=dc6zaTOxFJmzC&amp;limit=10</td>
---
+++ javascript/gamejust questions.js
@@ -0,0 +1,104 @@
+ $(document).ready(function() {
+ 
+ $('#gameshow, #endjumbotron').hide('slowly');//prep for game start screen
+
+
+$('#startjumbotron').click(function() {     //start game and timer
+    	$('#jumbotronone').hide('slowly');
+		  $('#gameshow').show('slowly');
+		      questionGenerator(); 
+        
+       
+});          
+
+
+      var ques = [
+        {"q1":"0.0 one jhrkjehflkjhddqjkjjriejqe",
+        "correct":"0 .1correct",
+        "answer1":"answer1",
+        "answer2":"answer2",
+        "answer3":"answer3"
+         },
+    
+       {"q1":"0.1 one jhrkjehflkjhddqjkjjriejqe",
+        "correct":"0 .2correct",
+        "answer1":"answer1",
+        "answer2":"answer2",
+        "answer3":"answer3"
+       },
+       ];
+
+       console.log(ques.length);
+
+function questionGenerator(){   
+       for(var i = 0; i <= ques.length; i++){
+        //console.log(ques[(i)]);
+        //console.log(ques[(i)].q1);
+         //console.log(ques.q1[0][1]);
+
+         var quesbox = $('<div class="questionbox">');  
+            var questiontxt = ques[(i)].q1;
+            quesbox.append(questiontxt); 
+
+         var ansbox = $('<div class="answerbox">');  
+        var answercorrect = ques[(i)].answer1; 
+        ansbox.append(answercorrect);
+        var rbtn = $('<input type="radio" name="radio_name" />');
+        rbtn.attr(<label for="radio_name">jen</label>);
+
+              $("#qContainer").append(quesbox, ansbox, rbtn);
+       }
+ };        
+  
+});
+
+
+/*
+
+    function questionSetup{
+
+            var quesbox = $('<div class="questionbox">');  
+            var questiontxt = ques[0] [0];
+            quesbox.append(questiontxt); 
+
+            var ansbox = $('<div class="answerbox">');  
+            var answercorrect = ques[0][1]; 
+            var answertxt1 = ques[0][2]; 
+            var answertxt2 = ques[0][3]; 
+            var answertxt3 = ques[0][4];
+            ansbox.append(answercorrect, answertxt1, answertxt2,answertxt3);   
+
+           $("#qContainer").append(quesbox, ansbox);
+      }
+*/
+
+ 
+/*
+function appendText() {
+    var txt1 = "<p>Text.</p>";              // Create text with HTML
+    var txt2 = $("<p></p>").text("Text.");  // Create text with jQuery
+    var txt3 = document.createElement("txt3cnt");
+    txt3.innerHTML = " created element 1Text";         
+
+    $("#gContainer").append(txt1, txt2, txt3);     
+  }
+//var questions = []; //stores questions and answers 
+
+//unction questionGenerator{
+//    var txt3 = document.createElement("p");
+  //  txt3.innerHTML = "Text.";   
+
+
+//}// writes questions and answers to screen, random order to questions, random order to answers right answer flag radio buttons for loops and 
+
+//questionTimer{}
+
+//
+
+//gameScore{}
+
+
+//show hide new element for total score, correct incorrect answer totals button for new game
+/*var triviaQuestions = {question:"question question question question question question question question question ", correct:"correct", choice1:"c1"choice2:"c2"choice3:"c3"};
+$("#gContainer").append(txt1, txt2, txt3);     // Append new elements
+}*/
